8a51606bb79f532f2348c8d7591beb0dc14c1830
lilly <lilly@itdadev.com>
2023-11-15 00:05:12 +0900

    commit after done with side bar, top bar, bottom bar and routing for each pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./_root/pages";
-import SignInForm from "./_auth/forms/SignInForm";
-import SignUpForm from "./_auth/forms/SignUpForm";
 
 import { Toaster } from "@/components/ui/toaster";
+
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
 
 import "./globals.css";
 import AuthLayout from "./_auth/AuthLayout";
@@ -19,11 +30,18 @@ export const App = () => {
           <Route path="/sign-up" element={<SignUpForm />} />
         </Route>
 
+        {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
-
-        {/* private routes */}
       </Routes>
 
       <Toaster />
